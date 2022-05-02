a2482cbdec9cf93f43116edfca30cf9a55e385c0
dain <57316771+DainPark-web@users.noreply.github.com>
2022-05-02 16:17:57 +0200

    update

--- client/js/p5js/ex01.js
+++ client/js/p5js/ex01.js
@@ -3,16 +3,24 @@ const mainContainer = document.querySelector(".p5container")
 let WIDTH = mainContainer.clientWidth;
 let HEIGHT = mainContainer.clientHeight;
 
+
+let img;
+function preload() {
+    img = loadImage("../client/js/p5js/assets/dain.jpg");
+  }
+
 function setup (){
     let canvas = createCanvas(WIDTH, HEIGHT);
     canvas.parent(mainContainer);
+    background(0);
     rectMode(CENTER);
-    
+    image(img, 0, 0, 400, 400)
 }
 
 function draw(){
-    background(0);
-    rect(width/2, height/2, 100, 100)
+    
+   
+    // rect(width/2, height/2, 100, 100)
 }
 
 function windowResized(){
