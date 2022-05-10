ab980f8e200ead72190d5760c60638dc1c5d9ab4
dain <57316771+DainPark-web@users.noreply.github.com>
2022-05-10 14:26:09 +0200

    update

--- client/js/p5js/ex04.js
+++ client/js/p5js/ex04.js
@@ -11,7 +11,7 @@ let imgH = 500;
 let ball;
 let balls = []
 
-let ballCount = 300;
+let ballCount = 800;
 
 function preload(){
     img = loadImage("../client/js/p5js/assets/test.jpg")
@@ -24,7 +24,7 @@ function setup (){
     for(let i = 0; i < ballCount; i++){
         const randomW = random(imgW);
         const h = HEIGHT/2 - (imgH/2);
-        balls.push(new Ball(randomW, h, WIDTH/2 - (imgW/2), imgW, HEIGHT/2 - (imgH/2), imgH))
+        balls.push(new Ball(randomW, h, WIDTH/2 - (imgW/2), imgW, HEIGHT/2 - (imgH/2), imgH, img))
     }
 
     background(0);
@@ -53,6 +53,7 @@ function setup (){
 function draw(){
     background(0);
     for(let i = 0; i < balls.length; i++){
+        
         balls[i].draw();
         balls[i].move();
 
--- client/js/p5js/ex04_ball.js
+++ client/js/p5js/ex04_ball.js
@@ -1,5 +1,5 @@
 class Ball{
-    constructor(x, y, wsp, imgW, hsp, imgH){
+    constructor(x, y, wsp, imgW, hsp, imgH, img){
         this.x = x + wsp;
         this.y = y;
         this.wStartPoint = wsp;
@@ -8,18 +8,21 @@ class Ball{
         this.hL = imgH;
         this.sy = random(1, 4)
         this.acc = random(0.1, 0.4);
+        this.img = img
+        this.col = [255, 255,255,255];
 
     }
 
     draw(){
-        fill(255)
+        fill(this.col)
         ellipse(this.x, this.y, 10, 10);
     }
 
     move(){
         this.sy += this.acc;
         this.y += this.sy;
-
+        const getC = this.img.get(this.x - imgW, this.y);
+        this.col = getC
         if(this.y > this.hStartPoint + this.hL){
             this.y = this.hStartPoint;
             this.sy = random(1, 4);
