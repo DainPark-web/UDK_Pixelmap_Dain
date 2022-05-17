758999a370456b1f35cf7216e785211438774b79
dain <57316771+DainPark-web@users.noreply.github.com>
2022-05-17 13:51:16 +0200

    update

--- client/js/pages/index.js
+++ client/js/pages/index.js
@@ -6,9 +6,15 @@ const homePathList = document.getElementById("home_pathList");
 for( let i = 0; i < pathListData.length; i++){
     const li = document.createElement("li");
     const a = document.createElement("a");
+    const span = document.createElement("span");
+    const div = document.createElement("div");
 
-    a.innerText = pathListData[i].title;
+    div.classList.toggle("black_screen")
+    span.innerText = pathListData[i].title;
     a.setAttribute("href", pathListData[i].path )
+    a.style.backgroundImage = `url(${pathListData[i].img})`
+    a.appendChild(span)
+    a.appendChild(div)
     li.appendChild(a);
     homePathList.appendChild(li);
 }
--- libs/listData.mjs
+++ libs/listData.mjs
@@ -1,19 +1,23 @@
 export const pathListData = [
     {
         title: "Experiment01",
-        path : "/pages/experiment01.html"
+        path : "/pages/experiment01.html",
+        img: "../client/css/assets/fotos/01.png"
     },
     {
         title: "Experiment02",
-        path : "/pages/experiment02.html"
+        path : "/pages/experiment02.html",
+        img: "../client/css/assets/fotos/01.png"
     },
     {
         title: "Experiment03",
-        path : "/pages/experiment03.html"
+        path : "/pages/experiment03.html",
+        img: "../client/css/assets/fotos/01.png"
     },
     {
         title: "Experiment04",
-        path : "/pages/experiment04.html"
+        path : "/pages/experiment04.html",
+        img: "../client/css/assets/fotos/01.png"
     },
   
    
