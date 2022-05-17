54955cb345b3b1ad038b5ad565913411daa91aa6
dain <57316771+DainPark-web@users.noreply.github.com>
2022-05-17 14:06:33 +0200

    update

--- libs/listData.mjs
+++ libs/listData.mjs
@@ -7,17 +7,17 @@ export const pathListData = [
     {
         title: "Experiment02",
         path : "/pages/experiment02.html",
-        img: "../client/css/assets/fotos/01.png"
+        img: "../client/css/assets/fotos/02.png"
     },
     {
         title: "Experiment03",
         path : "/pages/experiment03.html",
-        img: "../client/css/assets/fotos/01.png"
+        img: "../client/css/assets/fotos/03.png"
     },
     {
         title: "Experiment04",
         path : "/pages/experiment04.html",
-        img: "../client/css/assets/fotos/01.png"
+        img: "../client/css/assets/fotos/04.png"
     },
   
    
