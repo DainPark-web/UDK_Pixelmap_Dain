378809ddd764df946c5d4465356e968d09b53124
dain <57316771+DainPark-web@users.noreply.github.com>
2022-05-27 21:22:22 +0200

    update

--- libs/listData.mjs
+++ libs/listData.mjs
@@ -1,41 +1,41 @@
 export const pathListData = [
     {
-        title: "Experiment01",
+        title: "Random-Pixel",
         path : "/pages/experiment01.html",
         img: "../client/css/assets/fotos/01.png"
     },
     {
-        title: "Experiment02",
+        title: "Get(Pixel)",
         path : "/pages/experiment02.html",
         img: "../client/css/assets/fotos/02.png"
     },
     {
-        title: "Experiment03",
+        title: "Class-Pixel",
         path : "/pages/experiment03.html",
         img: "../client/css/assets/fotos/03.png"
     },
     {
-        title: "Experiment04",
+        title: "Mod-Pixel",
         path : "/pages/experiment04.html",
         img: "../client/css/assets/fotos/04.png"
     },
     {
-        title: "Experiment05",
+        title: "Pixel.j < Pixel.j+1",
         path : "/pages/experiment05.html",
         img: "../client/css/assets/fotos/05.png"
     },
     {
-        title: "Experiment06",
+        title: "P += 10",
         path : "/pages/experiment06.html",
         img: "../client/css/assets/fotos/06.png"
     },
     {
-        title: "Experiment07",
+        title: "Dist-Pixel",
         path : "/pages/experiment07.html",
         img: "../client/css/assets/fotos/07.png"
     },
     {
-        title: "Experiment08",
+        title: "Atan2-Pixel",
         path : "/pages/experiment08.html",
         img: "../client/css/assets/fotos/08.png"
     },
